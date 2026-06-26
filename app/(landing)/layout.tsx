55496614e17bec058044e913788fd78c81045128
detectivecrewze <aldoramadhan16@gmail.com>
2026-06-27 01:41:52 +0700

    perf(homepage): reduce GPU load - blur 24px->12px, disable mobile grain, lazy CartDrawer

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React from "react";
+import dynamic from "next/dynamic";
 import "./landing.css";
 import { CartProvider } from "../context/CartContext";
-import CartDrawer from "../components/CartDrawer";
-import CartToast from "../components/CartToast";
+
+const CartDrawer = dynamic(() => import("../components/CartDrawer"), { ssr: false });
+const CartToast = dynamic(() => import("../components/CartToast"), { ssr: false });
 
 export default function LandingLayout({
     children,
